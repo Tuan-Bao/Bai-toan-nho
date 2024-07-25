6e9161dc1e22c3931679112e645e5c29f4d91d75
Tuan-Bao <tuanbaoho2k3@gmail.com>
2024-07-25 15:11:39 +0700

    update file

--- push_data_in_DB.py
+++ push_data_in_DB.py
@@ -14,5 +14,30 @@ c = conn.cursor()
 c.execute   ("""CREATE TABLE IF NOT EXISTS league (
                 id_league INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT UNIQUE NOT NULL,
-                
-            )""")
+                code TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)""")
+
+# Tạo trigger để tự động cập nhật modified_at
+c.execute   ("""CREATE TRIGGER IF NOT EXISTS update_league_modified_at
+                AFTER UPDATE ON League
+                FOR EACH ROW
+                BEGIN
+                    UPDATE League SET modified_at = CURRENT_TIMESTAMP
+                    WHERE id_league = OLD.id_league;
+                END;""")
+
+# Đường dẫn đến file csv
+csv_path = "D:\\Cào dữ liệu\\bài toán nhỏ\\csv\\Premier League\\league name\\league_name.csv"
+
+# Đọc dữ liệu từ file csv và chèn vào bảng League
+with open(csv_path, "r") as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        name = row[0]
+        code = name[:3].upper()
+    c.execute("INSERT OR IGNORE INTO league (name, code) VALUES (?, ?)", (name, code))
+
+# Lưu các thay đổi và đóng kết nối
+conn.commit()
+conn.close()
